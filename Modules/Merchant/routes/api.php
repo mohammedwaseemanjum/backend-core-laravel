912f20d2258f78fab34cad559e766fa4e09b32ec
anjum <44854471+mohammedwaseemanjum@users.noreply.github.com>
2026-08-13 16:24:14 +0800

    intall moduelar architecture

---
+++ Modules/Merchant/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Merchant\Http\Controllers\MerchantController;
+
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('merchants', MerchantController::class)->names('merchant');
+});
+
+Route::get('merchant/test', function () {
+    return 1;
+});
